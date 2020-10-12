8adbc00609975f3f133468794cb269b47b4cb2b8
westcoastcoder <wwscottbc@gmail.com>
2020-10-12 15:38:20 -0700

    Cleaned up code and added a simple output test to let the user know if bacon was found.

--- main.py
+++ main.py
@@ -5,33 +5,34 @@ import re
 
 # Create Global Variables
 welcomeMsg = """
-Hello! Welcome to the BaconScraper. Please enter a wiki link that you'd like to start with. \n
+Hello! Welcome to the BaconScraper.
+Please enter a wiki link that you'd like to start with.
 """
 
 # Welcome user to program and request input
-print(welcomeMsg.lstrip())
+print(welcomeMsg)
 url = input()
 
 # request html from user url
 html = requests.get(url)
 
-# make our soup
+# Make our soup
 soup = BeautifulSoup(html.content, 'html.parser')
 
-# get the body content
-body = soup.find_all(id='bodyContent')
+# Pulls the actual text (not anchors/tags content) from the html using
+# the .get_text() function
+bodyCon = soup.find(id='bodyContent').get_text()
 
-bodyDiv = soup.find(id='bodyContent').get_text()
-bacFinder = re.compile(r'Bacon')
-bacon = bacFinder.findall(bodyDiv)
-bodyAnch = soup.find(id='bodyContent').find_all('a')
+# This is the regex used to search our soup for bacon
+bacFinder = re.compile(r'Kevin Bacon')
 
-#divBacon = bodyDiv.get_text()
-print(bacon)
-print("******************************************************")
-#print(bodyAnch)
-# check our soup for bacon
-# divBacon = body.find_all("div", string="Sedgwick was nominated", recursive=True)
-# tagBacon = body.find_all('a')
+# Search of the website's text for bacon
+bacon = bacFinder.findall(bodyCon)
 
-#print(body)
+# Let the user know if the page mentions KB at all
+if len(bacon) > 0:
+    print("\nKevin Bacon is mentioned on this page!\n")
+else:
+    print("\nIt doesn't look like Kevin Bacon is mentioned here...\n")
+
+# bodyAnch = soup.find(id='bodyContent').find_all('a')
